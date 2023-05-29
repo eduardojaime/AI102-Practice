057036ec3a3273a489a0a1dc9841ed701cab1f8f
Eduardo Jaime <eduardojaime86@gmail.com>
2023-05-29 10:12:51 -0400

    initial commit

---
+++ 1-language-services/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using System;
+
+namespace _1_language_services;
+
+class Program
+{
+    private static readonly AzureKeyCredential credentials = new AzureKeyCredential("<KEY>");
+    private static readonly Uri endpoint = new Uri("https://<servicename>.cognitiveservices.azure.com/");
+    static void Main(string[] args)
+    {
+        var client = new TextAnalyticsClient(endpoint, credentials);
+        SentimentAnalysisExample(client);
+        Console.ReadKey();
+    }
+
+    static void SentimentAnalysisExample(TextAnalyticsClient client) {
+        string inputText = "hello there how are things my friend!!!??";
+        DocumentSentiment docSentiment = client.AnalyzeSentiment(inputText);
+        Console.WriteLine($"Doc sentiment: {docSentiment.Sentiment}");
+    }
+}
